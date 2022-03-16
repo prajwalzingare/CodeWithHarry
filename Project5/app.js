e81d9ac0153824cbfa5ef718e9e9c0ce3a5ff7c4
prajwalzingare <74585612+prajwalzingare@users.noreply.github.com>
2022-03-16 11:04:53 +0530

    project 5

--- Project5/app.js
+++ Project5/app.js
@@ -9,4 +9,78 @@ const data = [
     framework: "React",
     image: "https://randomuser.me/api/portraits/men/55.jpg",
   },
+  {
+    name: "prajwal",
+    age: "24",
+    city: "Nagpur",
+    language: "Java",
+    framework: "php",
+    image: "https://randomuser.me/api/portraits/men/54.jpg",
+  },
+  {
+    name: "sami",
+    age: "21",
+    city: "Nagpur",
+    language: "Javascript",
+    framework: "React",
+    image: "https://randomuser.me/api/portraits/women/55.jpg",
+  },
+  {
+    name: "sami zingare",
+    age: "20",
+    city: "Nagpur",
+    language: "Javascript",
+    framework: "angular",
+    image: "https://randomuser.me/api/portraits/women/52.jpg",
+  },
+  {
+    name: "sourabh Pande",
+    age: "23",
+    city: "Nagpur",
+    language: "Javascript",
+    framework: "Vue",
+    image: "https://randomuser.me/api/portraits/men/40.jpg",
+  },
 ];
+//CV iterator
+function cvIterator(profiles) {
+  //we should intialize with zero
+  let nextindex = 0;
+  //we are returning an object which is function.
+  return {
+    next: function () {
+      return nextindex < profiles.length
+        ? { value: profiles[nextindex++], done: false }
+        : {
+            done: true,
+          };
+    },
+  };
+}
+
+const candidates = cvIterator(data);
+//pahle ku function call kiya kuki jub app starting me page load karoge with out click to ye function run hona chahiye isliye.
+nextCV();
+//Button listner for next button
+const next = document.getElementById("next");
+next.addEventListener("click", nextCV);
+function nextCV() {
+  const currentCandidate = candidates.next().value;
+  let image = document.getElementById("image");
+  let profile = document.getElementById("profile");
+  if (currentCandidate != undefined) {
+    image.innerHTML = `<img src="${currentCandidate.image}"/>`;
+    profile.innerHTML = `<ul class="list-group">
+    <li class="list-group-item">${currentCandidate.name}</li>
+    <li class="list-group-item">${currentCandidate.age}</li>
+    <li class="list-group-item">${currentCandidate.city}</li>
+    <li class="list-group-item">${currentCandidate.language}</li>
+    <li class="list-group-item">${currentCandidate.framework}</li>
+  </ul>`;
+  } else {
+    //After end of candidate it gives you alert
+    alert("End of candidate applications");
+    //it will relod the apllication back to original posiyion
+    window.location.replace("index.html");
+  }
+}
--- Tutorials/tut51.js
+++ Tutorials/tut51.js
@@ -1,16 +1,19 @@
 console.log("this tut is on the topic of Iterators in javascript");
 //Link to know more-https://www.codewithharry.com/videos/javascript-tutorials-in-hindi-51/
 //https://www.youtube.com/watch?v=2oU-DfdWM0c
+//Link for ++ operator-https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
+//2)video link-https://www.youtube.com/watch?v=d4W6-mphL7c
+
 //Iterators-it means repetation
-function fruitsIterator(values) {
+function fruitsIterator(prajwal) {
   let nextIndex = 0;
   //we will return object.
   return {
     next: function () {
-      if (nextIndex < values.length) {
+      if (nextIndex < prajwal.length) {
         //we will return below object.
         return {
-          value: values[nextIndex++],
+          value: prajwal[nextIndex++], //++ postfix increment returns the value before incremanting.Basically original value return krta hai increment hone ke pahle ki.and prefix operator kya karta hai sidha original value return karta hai increse hui naki main value.got the point.
           done: false,
         };
       } else {
@@ -22,15 +25,15 @@ function fruitsIterator(values) {
   };
 }
 
-const myArray = ["Apples", "Grapes", "Bhindi", "chikoo"];
+const myArray = ["Apple", "Grapes", "Bhindi", "chikoo"];
 // console.log("My array is " + myArray);
 //using the Iterator
 const fruits = fruitsIterator(myArray);
 console.log(fruits.next());
-console.log(fruits.next().value);
-console.log(fruits.next().value);
-console.log(fruits.next().value);
-console.log(fruits.next().value);
+console.log(fruits.next());
+// console.log(fruits.next().value);
+// console.log(fruits.next().value);
+// console.log(fruits.next().value);
 
 //if we want to iterate thruough any lopp we use many method like
 //while(),do..while(),for(),for..of(),for..in(),foreach(),map() and recently new method get add and that method is Itearators
